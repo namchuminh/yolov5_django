de9b2af600c149d341123f7810020d8328d70b1d
namchuminh <chuminhnamma@gmail.com>
2023-06-23 16:59:03 +0700

    update time load, allowed_hosts

--- yolov7_django/settings.py
+++ yolov7_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_vighj&7ml!o523jqrjg*=-p(hc^pk$#+$p0oq$aa=ett#&3_1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
