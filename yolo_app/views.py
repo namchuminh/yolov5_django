d1ea27d8ba2fabea8b4e9bdea94e9666e0bad50b
namchuminh <chuminhnamma@gmail.com>
2023-07-10 01:26:16 +0700

    canh bao thieu mu, quan ao, gang tay

--- yolo_app/views.py
+++ yolo_app/views.py
@@ -10,9 +10,15 @@ from django.conf import settings
 
 model = torch.hub.load('ultralytics/yolov5', 'custom', os.path.join(settings.BASE_DIR,'models\\best.pt'))  # custom trained model
 
-classes = ('Mu', 'Quan_Ao', 'Gang_Tay') # Thay thế bằng classes của mô hình train
+classes = ['Mu', 'Quan Ao', 'Gang Tay'] # Thay thế bằng classes của mô hình train
 
 detected = False
+
+warning = False
+
+mu, quanao, gangtay = False, False, False
+
+i = 0
 
 class VideoCamera(object):
     def __init__(self):
@@ -30,32 +36,57 @@ class VideoCamera(object):
 
         results = model(frame)
         
-        for box in results.xyxy[0]: 
+        global i, warning, mu, quanao, gangtay, curr_time
+        
+        curr_time = time.strftime("%H:%M:%S", time.localtime())
+        
+        if 'quan ao' in str(results):
+            quanao = True
+        elif 'quan ao' not in str(results):
+            quanao = False
+            warning = True
             
-            if box[5] is not None and (float(box[4]) * 100) >= 50:
+        if 'gang tay' in str(results):
+            gangtay = True
+        elif 'gang tay' not in str(results):
+            gangtay = False
+            warning = True
+        
+        if 'mu' in str(results):
+            mu = True
+        elif 'mu' not in str(results):
+            mu = False
+            warning = True
+        
+        for box in results.xyxy[0]: 
+
+            if box[5] is not None and (float(box[4]) * 100) >= 20:
                 
-                global xA, yA, xB, yB, label, accuracy, detected, curr_time, full_info
+                global xA, yA, xB, yB, label, accuracy, detected, full_info
                 xB = int(box[2])
                 xA = int(box[0])
                 yB = int(box[3])
                 yA = int(box[1])
                 accuracy = round(float(box[4] * 100),2)
                 label = classes[int(box[5])]
-                curr_time = time.strftime("%H:%M:%S", time.localtime())
                 full_info = results.pandas().xyxy[0].values.tolist()
             
                 frame = cv2.rectangle(frame, (xA, yA), (xB, yB), self.colors[int(box[5])], 1)
                 frame = cv2.putText(frame, classes[int(box[5])], (xA, yA - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
 
                 detected = True
-                
+            
         ret, jpeg = cv2.imencode('.jpg', frame)
         return jpeg.tobytes()
 
 def info_detect():
     
-    global detected
+    global detected, warning
     
+    if warning == True:
+        warning == False
+        yield 'data: {0}/{1}/{2}/{3}\n\n'.format(mu,quanao,gangtay,curr_time)
+        
     if detected == True:
         detected = False
         yield 'data: {0}/{1}/{2}/{3}/{4}/{5}/{6}/{7}\n\n'.format(xA,yB,xB,yB,accuracy,label,curr_time,full_info)
